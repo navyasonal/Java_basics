08cca90057acc64a9dd91ad825c58271ccd9304a
Navya Sonal Agarwal <98568612+navyasonal@users.noreply.github.com>
2022-05-03 12:32:53 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,12 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+        p1(5);
+    }
+    public static void p1(int n) {
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
